69eb2e78eee4026ebf69b0fbbc7cdcf247819bfc
Raymond Lau <kingwanlau@gmail.com>
2021-01-10 21:47:27 -0500

    Day 9 Part 2 Solution

---
+++ AdventOfCodeDay09_Part02.py
@@ -0,0 +1,40 @@
+
+def check_sum(target_sum,preamble):
+    found_pair = False
+    for num in preamble:
+        if target_sum - num in preamble:
+            found_pair = True
+            break
+    return found_pair
+
+def check_invalid_xmas(xmas,preamble_len):
+    start_point = 0
+    there_invalid_xmas = False
+    invalid_xmas = None
+    for start_point in range(0,len(xmas)-preamble_len):
+        if not check_sum(xmas[start_point+preamble_len],xmas[start_point:start_point+preamble_len]):
+            print(f"invalid sum {xmas[start_point+preamble_len]}")
+            print(f"preamble = {xmas[start_point:start_point+preamble_len]}")
+            there_invalid_xmas = True
+            invalid_xmas = xmas[start_point+preamble_len]
+            break
+    return there_invalid_xmas, invalid_xmas
+
+if __name__ == "__main__":
+    xmas = [int(number.strip()) for number in open("AdventOfCodeDay09.txt","r")]
+    there_invalid_xmas, invalid_xmas = check_invalid_xmas(xmas,25)
+    if there_invalid_xmas:
+        print(f"{invalid_xmas}")
+        i,j = 0,1
+        while i < len(xmas) and j < len(xmas):
+            current_sum = sum(xmas[i:j])
+            if current_sum == invalid_xmas:
+                print(f"{xmas[i:j]}")
+                print(f"min = {min(xmas[i:j])}")
+                print(f"max = {max(xmas[i:j])}")
+                print(f"result = {min(xmas[i:j])+max(xmas[i:j])}")
+                break                
+            elif  current_sum > invalid_xmas:
+                i += 1
+            else:
+                j += 1
